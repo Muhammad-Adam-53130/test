265e47bb81297c23eb8492eeab84d4f7d2579eb4
Muhammad-Adam-53130 <adam@icu.gov.my>
2025-01-27 10:42:10 +0800

    update---

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -5,19 +5,3 @@
         <p>This is my dashboard page.</p>
     </div>
 @endsection
-{{-- <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                <h1 class="bg-white p-6 text-center uppercase font-black text-3xl">Welcome</h1>
-                <p class="p-6 text-white text-center uppercase font-semibold">This is my dashboard page.</p>
-            </div>
-        </div>
-    </div>
-</x-app-layout> --}}
